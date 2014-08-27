4464c0418ee2e88148ba79f8fae93a3558c674bf
James Graham <james@hoppipolla.co.uk>
2014-08-27 12:45:27 +0100

    fixup! fixup! fixup! More changes for travis

--- clone.sh
+++ clone.sh
@@ -1,7 +1,8 @@
 echo "Cloning GH repository" &&
+echo $DESTINATION &&
 git clone https://$GIT_NAME:$GH_TOKEN@github.com/$USERNAME/$REPO.git $DESTINATION &> /dev/null &&
 cd $DESTINATION &&
+ls &&
 git checkout $DESTINATION_BRANCH &&
 echo `git rev-parse HEAD` &&
-ls &&
 echo "Clone succeeded"
